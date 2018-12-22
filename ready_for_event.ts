8409d3aba440b5d6e2ecfc2e59a4c269b66eb58a
ibisha <16367098+ibisha@users.noreply.github.com>
2018-12-22 23:07:59 +0900

    add edit calendar event method

--- ready_for_event.ts
+++ ready_for_event.ts
@@ -1,5 +1,27 @@
 const PROPERTY = PropertiesService.getScriptProperties()
 
+function readyForConference() {
+  const sheet = SpreadsheetApp.openById(PROPERTY.getProperty('MASTER_SHEET')).getSheetByName('フォーム作成');
+  const rownum = sheet.getLastRow();
+  let colnum = 1;
+
+  const conference = new Conference();
+  conference.date = new Date(sheet.getRange(rownum, colnum++).getValue().toString());
+  conference.name = sheet.getRange(rownum, colnum++).getValue().toString();
+  conference.email = sheet.getRange(rownum, colnum++).getValue().toString();
+  conference.title = sheet.getRange(rownum, colnum++).getValue().toString();
+  conference.description = sheet.getRange(rownum, colnum++).getValue().toString();
+  conference.target = sheet.getRange(rownum, colnum++).getValue().toString();
+  Logger.log(conference.toString());
+
+  const attendeeUrl = createAttendeeForm(conference);
+  createAttendeeMail(conference, attendeeUrl);
+  const enqueteUrl = createEnqueteForm(conference);
+  createEnqueteMail(conference, enqueteUrl);
+  addDirectoryAtSharedDrive(conference);
+  // addConfluencePage(conference);
+}
+
 function getJapaneseDayOfWeek(date: Date) {
   const japanese_week_of_day = ['日', '月', '火', '水', '木', '金', '土']
   const daynum = parseInt(Utilities.formatDate(date, 'JST', 'u'))
@@ -79,6 +101,35 @@ function createEnqueteForm(conference: Conference): string {
   return form.getPublishedUrl();
 }
 
+/**
+ * 入力された日付の該当イベントを取得
+ */
+function getEvent(conference: Conference): GoogleAppsScript.Calendar.CalendarEvent {
+  const calendar = CalendarApp.getCalendarById(PROPERTY.getProperty('CAL_ID'));
+  const events = calendar.getEventsForDay(conference.date);
+  for (let event of events) {
+    if (event.getTitle().indexOf(PROPERTY.getProperty('EVENT_NAME')) !== -1) {
+      return event;
+    }
+  }
+  // イベントがない場合
+  const startTime = new Date(conference.date.getFullYear(), conference.date.getMonth(), conference.date.getDay(), 12, 0, 0);
+  const endTime = new Date(conference.date.getFullYear(), conference.date.getMonth(), conference.date.getDay(), 13, 0, 0);
+  return calendar.createEvent(conference.title, startTime, endTime);
+}
+
+function editCalendarEvent(conference: Conference) {
+  const event = getEvent(conference);
+  event.setTitle(`講演「${conference.title}」`);
+  event.setDescription(`
+title
+${conference.title}
+  `);
+  event.addGuest(conference.email);
+  event.addGuest(PROPERTY.getProperty('GROUP_MAIL'));
+
+}
+
 function addDirectoryAtSharedDrive(conference: Conference): string {
   const rootDir = DriveApp.getFolderById(PROPERTY.getProperty('SHARE_DRIVE_ID'));
   const url = rootDir.createFolder(`${Utilities.formatDate(conference.date, "JST", "yyyyyMMdd")}_${conference.title}`);
@@ -98,26 +149,4 @@ function addConfluencePage(conference: Conference) {
   const response = UrlFetchApp.fetch(PROPERTY.getProperty('CONFLUENCE_URL'), options);
   const result = JSON.parse(response.getContentText());
   return result["url"];
-}
-
-function readyForConference() {
-  const sheet = SpreadsheetApp.openById(PROPERTY.getProperty('MASTER_SHEET')).getSheetByName('フォーム作成');
-  const rownum = sheet.getLastRow();
-  let colnum = 1;
-
-  const conference = new Conference();
-  conference.date = new Date(sheet.getRange(rownum, colnum++).getValue().toString());
-  conference.name = sheet.getRange(rownum, colnum++).getValue().toString();
-  conference.email = sheet.getRange(rownum, colnum++).getValue().toString();
-  conference.title = sheet.getRange(rownum, colnum++).getValue().toString();
-  conference.description = sheet.getRange(rownum, colnum++).getValue().toString();
-  conference.target = sheet.getRange(rownum, colnum++).getValue().toString();
-  Logger.log(conference.toString());
-
-  const attendeeUrl = createAttendeeForm(conference);
-  createAttendeeMail(conference, attendeeUrl);
-  const enqueteUrl = createEnqueteForm(conference);
-  createEnqueteMail(conference, enqueteUrl);
-  addDirectoryAtSharedDrive(conference);
-  // addConfluencePage(conference);
 }
